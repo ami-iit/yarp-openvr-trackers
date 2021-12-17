0e3d7017bd33f0524cda54acb33ab0d464818cc5
Diego Ferigo <diego.ferigo@iit.it>
2021-12-17 14:44:39 +0100

    Update C++ example of the driver

--- src/run_driver.cpp
+++ src/run_driver.cpp
@@ -10,20 +10,55 @@
 
 #include "OpenVRTrackersDriver.h"
 
+#include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <thread>
 
 int main(int /*argc*/, char** /*argv*/)
 {
+    // Open
+    std::cout << "[main] Opening the manager..." << std::endl;
     auto manager = openvr::DevicesManager();
+    std::cout << "[main] ... done" << std::endl;
 
+    // Initialize
+    std::cout << "[main] Initializing the manager..." << std::endl;
     if (!manager.initialize()) {
         std::cerr << "Failed to initialize the manager" << std::endl;
         return 1;
     }
+    std::cerr << "[main] ... done" << std::endl;
 
+    // List devices
     for (const auto& sn : manager.managedDevices()) {
         std::cout << "Found: " << sn << std::endl;
+    }
+
+    // Print devices positions
+    while (true) {
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        for (const auto& sn : manager.managedDevices()) {
+
+            // Get the pose. If not available, the returned optional value
+            // is empty
+            const auto& poseOpt = manager.pose(sn);
+
+            if (!poseOpt.has_value()) {
+                std::cerr << "Failed to get position" << std::endl;
+                continue;
+            }
+
+            // Get the value
+            const auto& pose = poseOpt.value();
+
+            // Print the pose
+            std::cerr << sn << ": " << pose.position[0] << ", "
+                      << pose.position[1] << ", " << pose.position[2]
+                      << std::endl;
+        }
     }
 
     return 0;
